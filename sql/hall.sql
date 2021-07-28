e98805aa96f550a2d2a06aed2bb6a01036fa601a
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 13:10:41 +0100

    TO-DO Amend Address_ID

--- sql/hall.sql
+++ sql/hall.sql
@@ -1,18 +1,16 @@
 CREATE TABLE IF NOT EXISTS `yahuas`.`hall` (
   `hall_name` VARCHAR(100) NOT NULL,
-  `address` VARCHAR(100) NOT NULL,
+  `address_id` INT NULL,
   `telno` VARCHAR(11) NOT NULL,
   `manager_no` VARCHAR(4) NOT NULL,
   `postcode` VARCHAR(8) NULL DEFAULT NULL,
   `hall_no` VARCHAR(3) NOT NULL,
-  `updated_by` VARCHAR(10) NULL DEFAULT 'admin',
-  `updated` DATE NULL,
+  `updated_by` VARCHAR(10) NOT NULL 'admin',
+  `updated` DATE NOT NULL DEFAULT CURRENT_DATE,
   PRIMARY KEY (`hall_no`),
-  INDEX `manager_no_idx` (`manager_no` ASC) VISIBLE,
+  INDEX `manager_no_idx` (`manager_no` ASC),
   CONSTRAINT `manager_no`
     FOREIGN KEY (`manager_no`)
     REFERENCES `yahuas`.`staff` (`staff_no`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
