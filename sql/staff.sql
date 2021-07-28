f5ff66456498e5cd12caad82092b81cb02f01a3f
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 13:07:23 +0100

    Update staff.sql

--- sql/staff.sql
+++ sql/staff.sql
@@ -5,14 +5,13 @@ CREATE TABLE IF NOT EXISTS `yahuas`.`staff` (
   `staff_gender` VARCHAR(10) NOT NULL,
   `title` VARCHAR(100) NOT NULL,
   `staff_no` VARCHAR(4) NOT NULL,
-  `updated_by` VARCHAR(10) NULL,
-  `updated` DATE NULL,
-  INDEX `fk_staff_office1_idx` (`office_id` ASC) VISIBLE,
+  `updated_by` VARCHAR(10) NOT NULL 'admin',
+  `updated` DATE NOT NULL DEFAULT CURRENT_DATE,
+  INDEX `fk_staff_office1_idx` (`office_id` ASC),
   PRIMARY KEY (`staff_no`),
   CONSTRAINT `fk_staff_office1`
     FOREIGN KEY (`office_id`)
     REFERENCES `yahuas`.`office` (`office_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
+
