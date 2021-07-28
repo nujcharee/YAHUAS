8a151ffb39ad05f888be740c0bb49ec391433f41
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 13:24:06 +0100

    Update office.sql

--- sql/office.sql
+++ sql/office.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS `yahuas`.`office` (
   `office_id` INT(4) NOT NULL,
   `office_title` VARCHAR(100) NOT NULL,
-  `address` VARCHAR(100) NOT NULL,
-  `postcode` VARCHAR(8) NOT NULL,
+  `addres_id` INT NULL,
   `telno` VARCHAR(11) NOT NULL,
   `updated_by` VARCHAR(10) NULL DEFAULT 'admin',
   `updated` DATE NOT NULL DEFAULT CURRENT_DATE,
