7bb5b71be97fcc1b06d7bc1e8cf36ffd5a900c39
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 12:58:38 +0100

    Update address.sql

--- sql/address.sql
+++ sql/address.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `yahuas`.`address` (
   `address_id` INT NOT NULL AUTO_INCREMENT,
   `addr1` VARCHAR(50) NULL,
-  `postcode` VARCHAR(8) NULL,
-  `updated_by` VARCHAR(10) NULL DEFAULT 'admin',
+  `postcode` VARCHAR(8) NOT NULL,
+  `updated_by` VARCHAR(10) NOT NULL DEFAULT 'admin',
   `updated` DATE NULL,
   PRIMARY KEY (`address_id`))
 ENGINE = InnoDB
