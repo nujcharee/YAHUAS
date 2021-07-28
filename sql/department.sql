0b05e025dd55081c9e42c659ba223ebbb5238a34
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 13:08:39 +0100

    Update department.sql

--- sql/department.sql
+++ sql/department.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`department` (
+CREATE TABLE IF NOT EXISTS `yahuas`.`department` (
   `dept_id` INT NOT NULL,
   `dept_name` VARCHAR(45) NULL,
   `office_id` INT NULL,
-  `updated` DATE NULL,
-  `updated_by` VARCHAR(10) NULL,
+  `updated_by` VARCHAR(10) NOT NULL 'admin',
+  `updated` DATE NOT NULL DEFAULT CURRENT_DATE,
   PRIMARY KEY (`dept_id`),
   INDEX `office_id_idx` (`office_id` ASC) VISIBLE,
   CONSTRAINT `office_id`
@@ -11,4 +11,3 @@ CREATE TABLE IF NOT EXISTS `mydb`.`department` (
     REFERENCES `yahuas`.`office` (`office_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
