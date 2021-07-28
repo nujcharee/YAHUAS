2e9b604145dc39efebe8f7b7e9d1701d5d578a92
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 13:11:40 +0100

    Update hall_room.sql

--- sql/hall_room.sql
+++ sql/hall_room.sql
@@ -3,14 +3,12 @@ CREATE TABLE IF NOT EXISTS `yahuas`.`hall_room` (
   `monthly_rent_rate` FLOAT NOT NULL,
   `place_no` VARCHAR(5) NOT NULL,
   `hall_no` VARCHAR(2) NULL,
-  `updated_by` VARCHAR(10) NULL,
-  `updated` DATE NULL,
+  `updated_by` VARCHAR(10) NOT NULL 'admin',
+  `updated` DATE NOT NULL DEFAULT CURRENT_DATE,
   PRIMARY KEY (`place_no`),
-  INDEX `hall_no_idx` (`hall_no` ASC) VISIBLE,
+  INDEX `hall_no_idx` (`hall_no` ASC),
   CONSTRAINT `hall_no`
     FOREIGN KEY (`hall_no`)
     REFERENCES `yahuas`.`hall` (`hall_no`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
