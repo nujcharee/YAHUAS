31e74070a593e493055f2a494dacb61fb9cf5c80
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 15:23:12 +0100

    Update hall_room.sql

--- hall_room.sql
+++ hall_room.sql
@@ -2,7 +2,7 @@ CREATE TABLE `hall_room` (
   `room_no` varchar(6) NOT NULL,
   `monthly_rent_rate` float NOT NULL,
   `place_no` varchar(16) NOT NULL,
-  `hall_no` varchar(2) NOT NULL
+  `hall_no` varchar(2) NOT NULL,
    `updated_by` VARCHAR(10) NOT NULL DEFAULT 'admin',
-  `updated` DATE NOT NULL,
+  `updated` DATE NOT NULL
 ) 
