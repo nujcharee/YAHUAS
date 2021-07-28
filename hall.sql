a7d13c2865c253bbecfee3910479e7b4173e1aea
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 15:19:31 +0100

    Update hall.sql

--- hall.sql
+++ hall.sql
@@ -1,63 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 19, 2021 at 09:38 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `YAHUAS`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hall`
---
-
 CREATE TABLE `hall` (
   `hall_no` varchar(2) NOT NULL,
   `hall_name` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL,
+  `address_id` INT NULL,
   `telno` varchar(11) NOT NULL,
   `manager_no` varchar(4) NOT NULL,
-  `postcode` varchar(8) DEFAULT NULL
+  `updated_by` VARCHAR(10) NOT NULL DEFAULT 'admin',
+  `updated` DATE NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `hall`
---
-ALTER TABLE `hall`
-  ADD PRIMARY KEY (`hall_no`),
-  ADD KEY `manager_no` (`manager_no`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `hall`
---
-ALTER TABLE `hall`
-  ADD CONSTRAINT `hall_ibfk_1` FOREIGN KEY (`manager_no`) REFERENCES `staff` (`staff_no`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
