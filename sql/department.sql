e000eaf8dc5c6a6386379c1d81b95f70cd1a1085
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 13:30:38 +0100

    Update department.sql

--- sql/department.sql
+++ sql/department.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `yahuas`.`department` (
   `updated_by` VARCHAR(10) NOT NULL DEFAULT 'admin',
   `updated` DATE NOT NULL DEFAULT CURRENT_DATE,
   PRIMARY KEY (`dept_id`),
-  INDEX `office_id_idx` (`office_id` ASC) VISIBLE,
+  INDEX `office_id_idx` (`office_id` ASC),
   CONSTRAINT `office_id`
     FOREIGN KEY (`office_id`)
     REFERENCES `yahuas`.`office` (`office_id`)
