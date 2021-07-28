a579de4f85ab5b971c093ebd5c9eb617e526e2e4
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 15:23:54 +0100

    Update invoice.sql

--- invoice.sql
+++ invoice.sql
@@ -1,65 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 19, 2021 at 09:38 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `YAHUAS`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `invoice`
---
-
 CREATE TABLE `invoice` (
   `invoice_no` varchar(14) NOT NULL,
   `lease_no` varchar(10) NOT NULL,
   `semester` varchar(50) NOT NULL,
   `pay_due` float NOT NULL,
   `student_id` varchar(10) NOT NULL,
-  `pay_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `invoice`
---
-ALTER TABLE `invoice`
-  ADD PRIMARY KEY (`invoice_no`),
-  ADD KEY `lease_no` (`lease_no`),
-  ADD KEY `student_id` (`student_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `invoice`
---
-ALTER TABLE `invoice`
-  ADD CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`lease_no`) REFERENCES `lease` (`lease_no`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `invoice_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `pay_date` date NOT NULL,
+   `updated_by` VARCHAR(10) NOT NULL DEFAULT 'admin',
+  `updated` DATE NOT NULL
+) 
