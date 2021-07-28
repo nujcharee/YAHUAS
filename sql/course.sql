a4c2910e3f34852010046d336cf2c6a373d58cdb
nujcharee <s_nujcharee@hotmail.com>
2021-07-28 12:57:30 +0100

    Update course.sql

--- sql/course.sql
+++ sql/course.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `yahuas`.`course` (
   `room_no` VARCHAR(3) NOT NULL,
   `dept_name` VARCHAR(100) NOT NULL,
   `dept_telno` VARCHAR(11) NOT NULL,
-  `updated_by` VARCHAR(10) NULL,
-  `updated` DATE NULL,
+  `updated_by` VARCHAR(10) NOT NULL DEFAULT 'admin',
+  `updated` VARCHAR(10) NOT NULL DEFAULT CURRENT_DATE,
   PRIMARY KEY (`course_no`),
   INDEX `instructor_id_idx` (`instructor_id` ASC) VISIBLE,
   CONSTRAINT `instructor_id`
